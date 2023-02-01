00791f79755946137ab272b6d7b76c657def608d
DarioCastel <lecastelnovo@gmail.com>
2023-02-01 12:05:08 -0300

    rutesv1

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+import {useRoutes, Outlet} from 'react-router-dom'
+
+function App() {
+  const routes = useRoutes([
+    {
+      path:"/",
+      element:<div>Home <Outlet></Outlet></div>,
+      children:[
+        {
+          index:true,
+          element: <div>Home Content</div>
+        },
+        {
+          path:"/login",
+          element:<div>Login</div>
+        },
+        {
+          path:"/dashboard",
+          element:<div>Dashboard</div>
+        },
+        {
+          path:"/settings",
+          element:<div>Settings</div>
+        }
+      ]
+    },
+  ])
+  
+  return routes; 
+
+}
+
+export default App
