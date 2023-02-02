2607f7d35b77cd799d1a6ecb3a951745f6e6bb68
DarioCastel <lecastelnovo@gmail.com>
2023-02-02 20:46:14 -0300

    context

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router";
+import AuthConsumer from "../assets/hooks/auth";
 
 const Home = () => {
+  const auth= AuthConsumer();
   return (
     <>
       <h2>Home component</h2>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link,NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav className="navConf">
         <ul>
             
-            <Link classname="letrasLink" to={'/'}><li>Home<i class="fa-solid fa-house-user"></i></li></Link>
-            <Link classname="letrasLink" to={'/login'}><li>Login<i class="fa-solid fa-user"></i></li></Link>
-            <Link classname="letrasLink" to={'/settings'}><li>Settings<i class="fa-solid fa-gear"></i></li></Link>
+            <Link to={'/'} style={{ textDecoration: 'none' }}><li>Home<i class="fa-solid fa-house-user"></i></li></Link>
+            <Link to={'/login'} style={{ textDecoration: 'none' }}><li>Login<i class="fa-solid fa-user"></i></li></Link>
+            <Link to={'/settings'} style={{ textDecoration: 'none' }}><li>Settings<i class="fa-solid fa-gear"></i></li></Link>
            
         </ul>
     </nav>
